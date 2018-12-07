304ae553ccdf5974d7253697f303a4ea73f03b74
Brian Aronowitz <brian.aronowitz@berkeley.edu>
2018-12-06 17:12:53 -0800

    adding camera image and menu funcitonality

--- Assets/menuManagement.cs
+++ Assets/menuManagement.cs
@@ -10,16 +10,27 @@ using UnityEngine;
 
 public class menuManagement : MonoBehaviour {
 
+    public GameObject vrCamera;
+    public GameObject clutch;
 
-
+    private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		
+        offset = new Vector3(0.3f, 0.7f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    // Set the global transform of the clutch to be the transform of the player plus a fixed transform
+    public void centerClutchUnderPlayer()
+    {
+        Debug.Log("center clutch under player");
+        clutch.transform.position = vrCamera.transform.position - offset;
+        clutch.transform.rotation = vrCamera.transform.rotation;
+    }
+
 }
