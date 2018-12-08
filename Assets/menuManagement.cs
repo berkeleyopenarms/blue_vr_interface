6627dc7911519ee1afda9d51af3b25d52401201e
Brian Aronowitz <brian.aronowitz@berkeley.edu>
2018-12-07 17:55:30 -0800

    pre submission push

--- Assets/menuManagement.cs
+++ Assets/menuManagement.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Valve.VR.InteractionSystem;
 
 
 /*
@@ -12,6 +12,10 @@ public class menuManagement : MonoBehaviour {
 
     public GameObject vrCamera;
     public GameObject clutch;
+    public GameObject menu;
+
+    public blueInputSystem leftHandInputs;
+    public blueInputSystem rightHandInputs;
 
     private Vector3 offset;
 
@@ -22,15 +26,25 @@ public class menuManagement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Debug.Log(leftHandInputs.getGrip());
+        //Debug.Log(rightHandInputs.getGrip());
+
+        if (leftHandInputs.getGrip())
+        {
+            menu.active = !menu.active;
+        }
+    }
 
     // Set the global transform of the clutch to be the transform of the player plus a fixed transform
     public void centerClutchUnderPlayer()
     {
         Debug.Log("center clutch under player");
-        clutch.transform.position = vrCamera.transform.position - offset;
-        clutch.transform.rotation = vrCamera.transform.rotation;
+        //Ensuring to only move in 
+        //move position in XY plane
+        clutch.transform.position = new Vector3(vrCamera.transform.position.x - offset.x, clutch.transform.position.y, vrCamera.transform.position.z - offset.z);
+
+        //Moving rotation in Y direction only
+        clutch.transform.rotation = Quaternion.Euler(0, vrCamera.transform.localEulerAngles.y, 0);
     }
 
 }
