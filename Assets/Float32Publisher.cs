49cf1f551e4d4132bf5c4e4ea17a2509c9ad02c2
BorenTsai <nicholastsai13@gmail.com>
2019-04-21 17:12:47 -0700

    Gripper functionality implemented

---
+++ Assets/Float32Publisher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class Float32Publisher:  Publisher<Messages.Standard.Float32>
+    {
+        public float messageData;
+        public blueInputSystem hand;
+
+        private Messages.Standard.Float32 message;
+
+        protected override void Start()
+        {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void InitializeMessage()
+        {
+            message = new Messages.Standard.Float32
+            {
+                data = messageData
+            };
+        }
+
+        private void Update()
+        {
+            float trigger;
+            if (hand.getPinch())
+            {
+                trigger = 1;
+            }
+            else
+            {
+                trigger = 0;
+            }
+            message.data = trigger;
+            Publish(message);
+        }
+    }
+}
+   
